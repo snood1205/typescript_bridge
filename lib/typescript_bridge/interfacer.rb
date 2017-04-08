21b89b48272eefb174f2a373e236c55aa62c20c1
Eli Sadoff <esadoff@ardiangroup.com>
2017-04-08 12:59:32 -0400

    Checking for validity of required attributes on struct#new

--- lib/typescript_bridge/interfacer.rb
+++ lib/typescript_bridge/interfacer.rb
@@ -7,19 +7,34 @@ class Interfacer
     @attributes = attributes
   end
 
-  def self.parse(interface_string)
-    iter = interface_string.split(/[\s{}]/).reject(&:empty?).to_enum
-    loop { raise StopIteration if iter.next == 'interface' }
-    name = iter.peek
-    attributes = iter.each_cons(2).map do |token, following|
-      next unless following.match?(/;$/)
-      optional = token.match?(/\?:$/)
-      attr_name = token[0...(optional ? -2 : -1)]
-      datatype = following[0...-1]
-      InterfaceAttribute.new(attr_name, datatype, optional)
-    end.compact
-    Interfacer.new(name, attributes)
+  class << self
+    def parse(interface_string)
+      iter = interface_string.split(/[\s{}]/).reject(&:empty?).to_enum
+      loop { raise StopIteration if iter.next == 'interface' }
+      name = iter.peek
+      attributes = iter.each_cons(2).map do |token, following|
+        next unless following.match?(/;$/)
+        optional = token.match?(/\?:$/)
+        attr_name = token[0...(optional ? -2 : -1)]
+        datatype = following[0...-1]
+        InterfaceAttribute.new(attr_name, datatype, optional)
+      end.compact
+      Interfacer.new(name, attributes)
+    end
   end
+
+  def to_struct
+    attribute_names = attributes.map(&:sym_name)
+    Struct.new(attribute_names) do
+      def initialize(options = {})
+        required_attr = attributes.reject(&:optional).map(&:sym_name)
+        validity_array = options.keys - required_attr
+        raise "Missing attribute #{validity_array[0]}" if validity_array.length == 1
+        raise "Missing attributes #{validity_array.map(&:to_s).join(', ')}" if validity_array.length > 1
+      end
+    end
+  end
+end
 end
 
 class InterfaceAttribute
@@ -29,5 +44,9 @@ class InterfaceAttribute
     @name = name
     @datatype = datatype
     @optional = optional
+  end
+
+  def sym_name
+    name.to_sym
   end
 end
